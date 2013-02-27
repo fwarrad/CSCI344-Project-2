d8692b197d309d167a9f340d8b7391019b695cc8
Fareed Warrad <fwarrad@unca.edu>
2013-02-27 09:16:46 -0500

    finished main webapp, index.html, app.css, and app.js. still need to integrate a jquery plugin.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -2,27 +2,33 @@ var $ = window.$;
 var ctwitter = window.ctwitter; 
 var console = window.console; 
 var main = function () {
-        "use strict";
-        var twitter = new ctwitter.CTwitter();
-        // ctwitter funtion start
-        twitter.stream("statuses/filter", { lang: "en", track: ["galaxy s3", "htc m7"] }, function (stream) {
+    "use strict";
+    var userChoice = null,
+        twitter = new ctwitter.CTwitter();
+    // set value to search and start ctwitter
+    $("#clientInputButton").click(function () {
+        userChoice = $("#clientInput").val();
+        $("#clientInputButton").hide();
+        $("#clientInput").hide();
+        $(".newSearch").show();
+        //display search term used
+        $("#twitterResults").html("<p> You entered: " + userChoice + "</p>");
+        // loop the twitter feed
+        twitter.stream("statuses/filter", { lang: "en", track: [userChoice] }, function (stream) {
             stream.on("data", function (tweet) {
-
-            //feeds out to html file
-                $("#twitter").delay(800000).fadeIn(900000).append("<ul>" + tweet.text + " </ul>");
+                // send twitter feed to html
+             $("#twitter").delay(800000).fadeIn(900000).append("<ul>" + tweet.text + " </ul>");
             // Prints 9 tweets at a time
-                if ($("ul").length > 9) {
-                    $("ul:first").fadeIn('slow');
-                    $("ul:first").remove();
-                    $("ul:first").fadeOut(500);
-                }
-
-            // feeds out to console. 
-                console.log(tweet.text);            
-            }); 
-        });
-
-    }
-
+            if ($("ul").length > 9) {
+              $("ul:first").fadeIn('slow');
+              $("ul:first").remove();
+              $("ul:first").fadeOut(500);
+            } // count running totals for matching search term to keywords
+                
+            });  // end of stream.on
+        }); // end of twitter.stream
+    }); // end of .click function
+};// end of main
 
 $(document).ready(main);
+
